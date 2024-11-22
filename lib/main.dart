7c4639498696bee89ed774c776e01a20e6f777be
UooUzzz <uuumonazzz@gmail.com>
2024-11-22 14:27:29 +0900

    settings

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:team4_groupproject/item_add_page/item_add_page.dart';
 import 'package:team4_groupproject/item_cart_page/item_cart_page.dart';
+import 'package:team4_groupproject/item_detail_page/item_detail_page.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -14,14 +16,16 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
+        fontFamily: 'Inter',
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
       routes: {
         '/item_add_page': (context) => const ItemAddPage(),
         '/item_cart_page': (context) => const ItemCartPage(),
+        '/item_detail_page': (context) => const ItemDetailPage(),
       },
-      initialRoute: '/item_add_page',
+      initialRoute: '/item_detail_page',
     );
   }
 }
