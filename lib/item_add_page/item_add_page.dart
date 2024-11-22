5e3ba54a3c18ba7b338e2506ebe885b855e0569c
Hamiric <do9870@naver.com>
2024-11-22 12:51:23 +0900

    Feat : "item_add_page ImagePicker 추가"

--- lib/item_add_page/item_add_page.dart
+++ lib/item_add_page/item_add_page.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:team4_groupproject/item_add_page/widget/dish_info_widget.dart';
 import 'package:team4_groupproject/item_add_page/widget/image_picker_widget.dart';
 import 'package:team4_groupproject/item_add_page/widget/register_button.dart';
 
-class ItemAddPage extends StatelessWidget {
+class ItemAddPage extends StatefulWidget {
   const ItemAddPage({super.key});
+
+  @override
+  State<ItemAddPage> createState() => _ItemAddPageState();
+}
+
+class _ItemAddPageState extends State<ItemAddPage> {
+  final ImagePicker imagePicker = ImagePicker();
+  XFile? selectedImage;
 
   @override
   Widget build(BuildContext context) {
@@ -22,9 +31,17 @@ class ItemAddPage extends StatelessWidget {
           },
           child: ListView(
             scrollDirection: Axis.vertical,
-            children: const [
-              ImagePickerWidget(),
-              DishInfoWidget(),
+            children: [
+              ImagePickerWidget(
+                imagePicker: imagePicker,
+                selectedImage: selectedImage,
+                imageSelected: (image) {
+                  setState(() {
+                    selectedImage = image;
+                  });
+                },
+              ),
+              const DishInfoWidget(),
             ],
           ),
         ),
