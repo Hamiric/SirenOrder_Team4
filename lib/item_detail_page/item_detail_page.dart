3163377983e519f64ce39bfc1eb41065cf9724d6
UooUzzz <uuumonazzz@gmail.com>
2024-11-25 21:56:38 +0900

    url feature success

--- lib/item_detail_page/item_detail_page.dart
+++ lib/item_detail_page/item_detail_page.dart
@@ -45,7 +45,7 @@ class _ItemDetailPageState extends State<ItemDetailPage> {
           final uri = Uri.parse(url); // String -> Uri 변환
 
           if (await canLaunchUrl(uri)) {
-            await launchUrl(uri);
+            await launchUrl(uri, mode: LaunchMode.externalApplication); // 외부 브라우저에서 여는 방식
           } else {
             throw 'Could not launch $url';
           }
